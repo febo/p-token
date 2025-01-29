b470f0a06b84a8bdde2844c555034a1c2475998f
febo <febo@anza.xyz>
2025-01-29 03:10:50 +0000

    Refactor

--- program/src/processor/batch.rs
+++ program/src/processor/batch.rs
@@ -1,28 +1,48 @@
-use pinocchio::{
-    account_info::AccountInfo, program_error::ProgramError, ProgramResult
-};
+use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
 
 use crate::entrypoint::process_instruction;
 
-#[inline(always)]
-pub fn process_batch(accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
-    // Validates the instruction data.
-    let (counter, mut instruction_data) = instruction_data
-        .split_first()
-        .ok_or(ProgramError::InvalidInstructionData)?;
+/// The size of the batch instruction header.
+///
+/// The header of each instruction consists of two `u8` values:
+///  * number of the accounts
+///  * length of the instruction data
+const IX_HEADER_SIZE: usize = 2;
 
-    let mut lengths: &[u8];
-    let mut accounts = accounts;
-    let mut current_accounts: &[AccountInfo];
-    let mut current_instruction_data: &[u8];
+pub fn process_batch(mut accounts: &[AccountInfo], mut instruction_data: &[u8]) -> ProgramResult {
+    loop {
+        // Validates the instruction data and accounts offset.
 
-    for _ in 0..*counter {
-        (lengths, instruction_data) = instruction_data
-            .split_at_checked(2)
-            .ok_or(ProgramError::InvalidInstructionData)?;
-        (current_accounts, accounts) = accounts.split_at_checked(lengths[0].into()).ok_or(ProgramError::InvalidInstructionData)?;
-        (current_instruction_data, instruction_data) = instruction_data.split_at_checked(lengths[1].into()).ok_or(ProgramError::InvalidInstructionData)?;
-        process_instruction(&token_interface::program::ID, current_accounts, current_instruction_data)?;
+        match instruction_data.len() {
+            0 => break,
+            n if n < IX_HEADER_SIZE => {
+                // The instruction data must have at least two bytes.
+                return Err(ProgramError::InvalidInstructionData);
+            }
+            _ => (),
+        }
+        // SAFETY: The instruction data is guaranteed to have at least two bytes.
+        let expected_accounts = unsafe { *instruction_data.get_unchecked(0) as usize };
+        let data_offset = IX_HEADER_SIZE + unsafe { *instruction_data.get_unchecked(1) as usize };
+        if instruction_data.len() < data_offset {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+        if accounts.len() < expected_accounts {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        }
+
+        // Process the instruction.
+
+        process_instruction(
+            &token_interface::program::ID,
+            &accounts[..expected_accounts],
+            &instruction_data[IX_HEADER_SIZE..data_offset],
+        )?;
+
+        accounts = &accounts[expected_accounts..];
+        instruction_data = &instruction_data[data_offset..];
     }
+
     Ok(())
 }
